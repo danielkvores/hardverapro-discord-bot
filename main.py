ea13f851a7b413a32457a2e67be3d8666d4f5625
danielkvores <kvoresdaniel@gmail.com>
2025-01-05 23:07:21 +0100

    minor adjustments

--- main.py
+++ main.py
@@ -32,13 +32,19 @@ soup = BeautifulSoup(html_content, "lxml")
 
 price_elements = soup.find_all("div", class_="uad-price")
 listing_elements = soup.find_all("div", class_="uad-title")
-other_elements = soup.find_all("span", class_="uad-rating")
+review_elements = soup.find_all("span", class_="uad-rating")
+other_elements = soup.find_all("div", class_="uad-light")
+location_and_username = [other_element.text.strip() for other_element in other_elements]
+
 
 
 data = {
     "Listing name": [listing_element.text.strip() for listing_element in listing_elements],
     "Price": [price_element.text.strip() for price_element in price_elements],
-    "Reviews": [other_element.text.strip() for other_element in other_elements]
+    "Reviews": [review_element.text.strip() for review_element in review_elements],
+    "Username": [location_and_username[i] for i in range(1, len(location_and_username), 3)],
+    "Location": [location_and_username[i] for i in range(0, len(location_and_username), 3)]
+
 }
 
 df = pd.DataFrame(data)
@@ -49,7 +55,7 @@ rich_display_dataframe(df, title="Hardverapro")
 idea: make this run constantly and update every 15 minutes f.e.
 and make the new listings appear in a different colour thanks to rich
 
-idea2: add cli arguments with which you can specify price ranges, etc.
+idea2: add cli arguments with which you can specify price ranges, etc., sort by what, etc
 
 idea3: maybe it could also check whether you have new notifications,
 and make that the text in the title of the dataframe, like
