0736ea83ea69d0822a347da9f17a811127c33085
danielkvores <kvoresdaniel@gmail.com>
2025-01-06 10:06:18 +0100

    polished a couple of things

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-import requests
-from bs4 import BeautifulSoup
-import time
-import lxml
+import requests # to get URL
+from bs4 import BeautifulSoup # to scrape the website
+import time # unused, will be used to make it wait
+import lxml # for bs4
 import pandas as pd
-import numpy
-import openpyxl
+import numpy # unused, might use later
+import openpyxl # used for exporting to excel
+
 
 def rich_display_dataframe(df, title):
+    """Function do display the data nicely for CLI. Not needed for excel, etc."""
     import contextlib
     from rich import print
     from rich.table import Table
     from rich.errors import NotRenderableError
+
     df = df.astype(str)
     table = Table(title=title)
     for col in df.columns:
@@ -37,27 +40,20 @@ other_elements = soup.find_all("div", class_="uad-light")
 location_and_username = [other_element.text.strip() for other_element in other_elements]
 
 
-
 data = {
-    "Listing name": [listing_element.text.strip() for listing_element in listing_elements],
+    "Listing name": [
+        listing_element.text.strip() for listing_element in listing_elements
+    ],
     "Price": [price_element.text.strip() for price_element in price_elements],
     "Reviews": [review_element.text.strip() for review_element in review_elements],
-    "Username": [location_and_username[i] for i in range(1, len(location_and_username), 3)],
-    "Location": [location_and_username[i] for i in range(0, len(location_and_username), 3)]
-
+    "Username": [
+        location_and_username[i] for i in range(1, len(location_and_username), 3)
+    ],
+    "Location": [
+        location_and_username[i] for i in range(0, len(location_and_username), 3)
+    ],
 }
 
 df = pd.DataFrame(data)
 
 rich_display_dataframe(df, title="Hardverapro")
-
-"""
-idea: make this run constantly and update every 15 minutes f.e.
-and make the new listings appear in a different colour thanks to rich
-
-idea2: add cli arguments with which you can specify price ranges, etc., sort by what, etc
-
-idea3: maybe it could also check whether you have new notifications,
-and make that the text in the title of the dataframe, like
-"x unread messages"
-"""
---
+++ todo.md
@@ -0,0 +1,16 @@
+- task: make this run constantly and update every 15 minutes f.e.
+and make the new listings appear in a different colour thanks to rich
+
+- task: add cli arguments with which you can specify price ranges, etc., sort by what, etc
+
+- task: maybe it could also check whether you have new notifications,
+and make that the text in the title of the dataframe, like
+"x unread messages"
+
+- task: be able to read the descriptions of the listings, and show link maybe
+how -> extract the link using soup, put it in a list, and make a requests.get() on the URL of the listing
+which contains the description
+
+- task: be able to choose between a couple of preset URLs, like [1] for macbooks, etc.
+
+- taks: add error messages
